2c05fe45cb66f4cd70ef7bc17bc5a29112ac0935
Deivison Soares <deivisonsoares1610@outlook.com>
2022-03-02 16:25:42 -0300

    att

--- exercicios/ex007/ambiente.js
+++ exercicios/ex007/ambiente.js
@@ -3,3 +3,9 @@ num.push(1)
 num.sort()
 console.log(`Nosso vetor é o ${num.length} posições`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
+let pos = num.indexOf(8)
+if(pos == -1){
+    console.log('O valor não foi encontrado!')
+}else{
+    console.log(`O valor 8 está na posição ${pos}`)
+}
