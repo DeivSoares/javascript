4afd16b0e13ddec31aa95c303d92f5884e3a90c6
Deivison Soares <deivisonsoares1610@outlook.com>
2022-03-13 21:37:08 -0300

    Finished

--- desafios/teste/script.js
+++ desafios/teste/script.js
@@ -1,11 +1,14 @@
 var cont = 1
-        var qntdFam = Number(prompt('Quantos membros tem sua familia?')) //Quantidade de Familiares 
+var totalIdade = 0
+        //Quantidade de Familiares 
+        var qntdFam = Number(prompt('Quantos membros tem sua familia?')) 
+
         while(cont<=qntdFam){
-        var idade = Number(prompt('Insira a idade dos familiares')) // Idade dos Familiares 
-        var totalIdade = 0
-        var totalIdade = totalIdade+idade
+        // Idade dos Familiares        
+        totalIdade += Number( prompt( 'Insira a idade dos familiares' ))
         cont++
         }
-        var media = totalIdade / qntdFam //Média = Todas as Idades Somadas Dividido pela Quantidade de Membros
+        //Média = Todas as Idades Somadas Dividido pela Quantidade de Membros
+        var media = totalIdade / qntdFam 
         alert(`A idade média da sua familia é ${Number.parseInt(media)} anos.`)
         res.innerHTML = `A idade média da sua familia é ${Number.parseInt(media)} anos.`
